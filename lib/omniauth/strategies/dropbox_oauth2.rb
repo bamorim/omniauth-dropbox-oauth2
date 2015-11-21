35743bb095aeda8896a1a02d71a79589a8efb3d7
Robert Kästel <robert@kastel.se>
2015-11-21 02:46:28 +0100

    Dropbox doesn't seem to like getting the redirect_uri in the token request with query string parameters in the uri.

--- lib/omniauth/strategies/dropbox_oauth2.rb
+++ lib/omniauth/strategies/dropbox_oauth2.rb
@@ -28,11 +28,15 @@ module OmniAuth
         @raw_info ||= MultiJson.decode(access_token.get('/1/account/info').body)
       end
 
+      def callback_url_without_query_string
+        full_host + script_name + callback_path
+      end
+
       def callback_url
         if @authorization_code_from_signed_request
           ''
         else
-          options[:callback_url] || super
+          options[:callback_url] || callback_url_without_query_string
         end
       end
     end
