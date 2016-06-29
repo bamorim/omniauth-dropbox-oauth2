9cbe6e4d0cd9698443eb80ecb40c51fa01415cfa
Bernardo Dornellas Cysneiros Gomes de Amorim <bamorim2@gmail.com>
2016-06-29 10:45:48 -0300

    Use Dropbox API v2
    Fixes #7
    Since uid format was changed, we should add a note for migration and on the README file.
    Also I'll release it on a new major version.

--- lib/omniauth/strategies/dropbox_oauth2.rb
+++ lib/omniauth/strategies/dropbox_oauth2.rb
@@ -6,16 +6,16 @@ module OmniAuth
       option :name, "dropbox_oauth2"
       option :client_options, {
         :site               => 'https://api.dropbox.com',
-        :authorize_url      => 'https://www.dropbox.com/1/oauth2/authorize',
-        :token_url          => 'https://api.dropbox.com/1/oauth2/token'
+        :authorize_url      => 'https://www.dropbox.com/oauth2/authorize',
+        :token_url          => 'https://api.dropbox.com/oauth2/token'
       }
 
       uid { raw_info['uid'] }
 
       info do
         {
-          'uid'   => raw_info['uid'],
-          'name'  => raw_info['display_name'],
+          'uid'   => raw_info['account_id'],
+          'name'  => raw_info['name']['display_name'],
           'email' => raw_info['email']
         }
       end
@@ -25,7 +25,7 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= MultiJson.decode(access_token.get('/1/account/info').body)
+        @raw_info ||= MultiJson.decode(access_token.get('/2/users/get_current_account').body)
       end
 
       def callback_url
